d5e93467cc19aa39d7ccaa4394fa0ecda273a3dc
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-08-22 22:53:33 +0300

    server.php

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const btns = document.querySelectorAll(".btn"); 
 const modal = document.getElementById('modal');
 const close = document.getElementById('close_btn');
+const submit = document.querySelector(".submit"); 
 
 
 btns.forEach(btn => {
@@ -11,4 +12,21 @@ btns.forEach(btn => {
 
 close.addEventListener('click', function() {
    modal.classList.remove('modal_opened') 
+})
+
+submit.addEventListener('click', async evt => {
+
+   evt.preventDefault()   
+
+   const formData = new formData()
+   formData.append('name', document.querySelector('#name').value)
+   formData.append('phone', document.querySelector('#phone').value)
+
+
+   let result = await responce.json();
+   console.log(result.res)
+   if(result.res == 'falid'){
+      document.querySelector('modal_content h2');
+      h2.textContant = 'Попробуйте еще раз'
+}
 })
